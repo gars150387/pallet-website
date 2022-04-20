2a00cd352f9e8ee5e0cffc28441d81b772e2455a
Gustavo <gar.santeliz@gmail.com>
2022-04-19 20:01:49 -0400

    re organized rendered component back to the beginning

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import React, { useReducer, useState } from 'react';
-import { AuthContext } from './auth/authContext';
+import React  from 'react';
 import { RouteMain } from './route/RouteMain';
-import { authReducer} from './auth/authReducer'
-
-
-const init = () => {
-  return {
-    language: true
-  }
-}
-
 
 function App() {
 
-  const [choice, setchoice] = useState(init)
-  const [state, dispatch] = useReducer(authReducer, {} , init ) 
-
   return (
     <div className="App">
-        <AuthContext.Provider value={{
-          state,
-          dispatch
-        }}>
-          <RouteMain />
-        </AuthContext.Provider>
+      <RouteMain />
     </div>
   );
 }
