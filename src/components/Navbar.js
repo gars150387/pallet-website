d9051fe15dd2c52e190510d892e1e08dcee75069
Gustavo <gar.santeliz@gmail.com>
2022-04-20 20:21:25 -0400

    font size changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,8 +16,8 @@ import '../style/navbar.css'
 export const NavbarWebsite = () => {
 
     return (
-        <div className="mb-2">
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" >
+        <div id="navbar" className="mb-2">
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
             </Navbar>
             <Navbar id="secondNavbar" collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
@@ -37,9 +37,9 @@ export const NavbarWebsite = () => {
                         </Nav>
                         <Nav>
 
-                            <Nav.Link eventKey={2} target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/Pallet305"><FaFacebookF id='icons' /></Nav.Link>
-                            <Nav.Link eventKey={2} target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/pallet_305/"><FaInstagram id='icons' /></Nav.Link>
-                            <Nav.Link eventKey={2} target="_blank" rel="noopener noreferrer" href="https://wa.me/message/SXDE7GX6G4OUF1"><ImWhatsapp id='icons' /></Nav.Link>
+                            <Nav.Link eventKey={2} target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/Pallet305"><FaFacebookF id='icons' style={{fontSize: "35px"}} /></Nav.Link>
+                            <Nav.Link eventKey={2} target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/pallet_305/"><FaInstagram id='icons' style={{fontSize: "40px"}}  /></Nav.Link>
+                            <Nav.Link eventKey={2} target="_blank" rel="noopener noreferrer" href="https://wa.me/message/SXDE7GX6G4OUF1"><ImWhatsapp id='icons' style={{fontSize: "35px"}}  /></Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
