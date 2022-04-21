7327d27bc801eae7bdbf64302a3819ad78bbd4ca
Gustavo <gar.santeliz@gmail.com>
2022-04-20 20:20:24 -0400

    items changed and replaced

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,6 @@ import { ImWhatsapp } from 'react-icons/im'
 import { CgCopyright } from 'react-icons/cg'
 import { Navbar, Container, Row, Col } from "react-bootstrap";
 
-import { FaCcVisa, FaCcDiscover, FaCcMastercard } from 'react-icons/fa';
-import { SiChase } from 'react-icons/si';
 
 import '../style/footer.css'
 
@@ -20,44 +18,32 @@ export const Footer = () => (
 						<Row style={{height: "10px"}}></Row>
 						<Col md={ 6 }>
 							<Link to="./about">
-								<h5 >About</h5>
+								<h5 style={{borderBottom: "solid 1px white"}} >About</h5>
 							</Link>
 							<Link to="./design">
-								<h5>Design</h5>
+								<h5 style={{borderBottom: "solid 1px white"}} >Design</h5>
 							</Link>
 							<Link to="./contact">
-								<h5>Contact</h5>
+								<h5 style={{borderBottom: "solid 1px white"}} >Contact</h5>
 							</Link>
 						</Col>
-						{/* <Col></Col> */}
 					</Row>
 					<Col md={ 4 }>
-						<Row xs="auto">
-							<Col xs lg=""><a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/Pallet305"><FaFacebookF id='iconsFooter' /></a></Col>
-							<Col xs lg=""><a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/pallet_305/"><FaInstagram id='iconsFooter' /></a></Col>
-							<Col xs lg=""><a target="_blank" rel="noopener noreferrer" href="https://wa.me/message/SXDE7GX6G4OUF1"><ImWhatsapp id='iconsFooter' /></a></Col>
+						<br />
+						<Row style={{ height: "15px", color: "white" }}> <h5>CONNECT TO US</h5></Row>
+						<Row xs={6} className="mt-3 align-justify">
+							<Col style={{border: "solid 2px white", borderRadius: "15px",marginRight: "3px", fontSize: "30px", padding: "auto"}}><a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/Pallet305"><FaFacebookF id='iconsFooter' /></a></Col>
+							<Col className="justify-content-center" style={{border: "solid 2px white", borderRadius: "15px",marginRight: "3px", fontSize: "35px"}}><a className="justify-content-center" target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/pallet_305/"><FaInstagram id='iconsFooter' /></a></Col>
+							<Col className="justify-content-center" style={{border: "solid 2px white", borderRadius: "15px",marginRight: "3px",fontSize: "29px"}}><a target="_blank" rel="noopener noreferrer" href="https://wa.me/message/SXDE7GX6G4OUF1"><ImWhatsapp id='iconsFooter' /></a></Col>
 						</Row>
 						<br />
-						<Row style={{ height: "15px", color: "white" }}> <h5>We Accept:</h5></Row>
-						<Row className="mt-3 align-justify">
-							<Col><FaCcVisa id="iconsFooterPayment"  style={{ color: "white"}} /></Col>
-							<Col><FaCcDiscover id="iconsFooterPayment"  style={{ color: "white"}} /></Col>
-							<Col><FaCcMastercard id="iconsFooterPayment"  style={{ color: "white"}} /></Col>
-							<Col><SiChase id="iconsFooterPayment"  style={{ color: "white"}} /></Col>
-							{/* <Col xs={2} ><FaMoneyBillAlt style={{ color: "white"}} /></Col> */}
-						</Row>
-						<br />
-
-
 					</Col>
 				</Container>
 			</Navbar>
-			<Row></Row>
 			<Row>
-				<Col></Col>
-				<Col xs={5}>
+				<Col xs lg={12}>
 					<Row className="justify-content-md-center">
-						<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="bottom">
+						<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
 							<Container className="justify-content-center">
 								<Navbar.Text>
 										<Col><p><CgCopyright /> 2022 Pallet305. All rights reserved. </p></Col>
@@ -66,7 +52,6 @@ export const Footer = () => (
 						</Navbar>
 					</Row>
 				</Col>
-				<Col></Col>
 			</Row>
 		</Container>
 	</div>
