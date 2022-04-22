5b7b213aef4ff229d521a1ade7c0bd11c48e5461
Gustavo <gar.santeliz@gmail.com>
2022-04-22 19:43:27 -0400

    fixing src path

--- src/components/DesignTemplate.js
+++ src/components/DesignTemplate.js
@@ -7,7 +7,7 @@ export const DesignTemplate = ({ img }) => {
     return (
         <Col xs lg={12} className="m-2 justify-content-center animate__animated animate__fadeInDownBig" id="template">
             <Col className="m-1 justify-content-center animate__animated animate__fadeInRightBig" id="template">
-                <Card.Img className="justify-content-center" src={require(`../img/${img}.jpg`)} alt={img} id="designImg" />
+                <Card.Img className="justify-content-center" src={require(`../img/${img}`)} alt={img} id="designImg" />
             </Col>
         </Col>
     )
