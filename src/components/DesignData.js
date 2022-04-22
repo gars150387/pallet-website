c588d49312d31c7a6e101468c75512405c29151e
Gustavo <gar.santeliz@gmail.com>
2022-04-22 19:42:38 -0400

    structuring data with right path

--- src/components/DesignData.js
+++ src/components/DesignData.js
@@ -9,58 +9,58 @@ export const DesignData = [
 {
     id: '02',
     img: 'pergola/pergola (1).jpg',
-    type: ''
+    type: 'pergola'
 },
 {
     id: '03',
     img: 'pergola/pergola (2).jpeg',
-    type: ''
+    type: 'pergola'
 },
 {
     id: '04',
     img: 'pergola/pergola (3).jpeg',
-    type: ''
+    type: 'pergola'
 },
 {
     id: '05',
     img: 'pergola/pergola (4).jpeg',
-    type: ''
+    type: 'pergola'
 },
 {
     id: '06',
     img: 'pergola/pergola (5).jpeg',
-    type: ''
+    type: 'pergola'
 },
 {
     id: '07',
     img: 'pergola/pergola (6).jpeg',
-    type: ''
+    type: 'pergola'
 },{
     id: '08',
     img: 'pergola/pergola (7).jpeg',
-    type: ''
+    type: 'pergola'
 },{
     id: '09',
     img: 'pergola/pergola (8).jpeg',
-    type: ''
+    type: 'pergola'
 },{
     id: '10',
     img: 'pergola/pergola (9).jpeg',
-    type: ''
+    type: 'pergola'
 },{
     id: '11',
     img: 'pergola/pergola (10).jpeg',
-    type: ''
+    type: 'pergola'
 },{
     id: '12',
     img: 'pergola/pergola (11).jpeg',
-    type: ''
+    type: 'pergola'
 },{
     id: '13',
     img: 'pergola/pergola (12).jpeg',
-    type: ''
+    type: 'pergola'
 },{
     id: '14',
     img: 'pergola/pergola (13).jpeg',
-    type: ''
+    type: 'pergola'
 }]
