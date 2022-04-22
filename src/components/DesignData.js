289997bdd7a06a49ad07023552b45b08fe51f1ca
Gustavo <gar.santeliz@gmail.com>
2022-04-22 19:41:46 -0400

    structuring data with right path

--- src/components/DesignData.js
+++ src/components/DesignData.js
@@ -1,85 +1,66 @@
-import '../img/2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85.jpg'
-import '../img/18A20FB2-818E-4B72-BE1E-BDECCC52134D.jpg'
-import '../img/58E5E329-E64F-4972-9392-A6CEAC17A913.jpg'
-import '../img/66B913C7-B3DC-4FE5-93CC-D4AD67EB9379.jpg'
-import '../img/29D529C0-9834-4926-A781-C6C881C0A2DE.jpg'
-import '../img/69AFD7B7-FA23-4A54-AB51-7234A6795F2E.jpg'
-import '../img/142B2076-642E-4451-8A54-F00F6822324A.jpg'
-import '../img/567F3275-1455-4384-ABCC-53263531A3E1.jpg'
-import '../img/85793fa7-72fb-492f-bc21-6a6def86bcf1.jpg'
-import '../img/2562834a-fd18-45d3-b519-9edc954a3acd.jpg'
-import '../img/61556755-B9B9-4DB0-8162-B38A736A693D.jpg'
-import '../img/A9927117-9F9A-44C2-9BEF-35876E24C7DF.jpg'
-import '../img/E2F7BDFE-FE72-48E0-B12F-441DEFEA0DC9.jpg'
-import '../img/f358a545-0748-4364-8c33-75d0764380f2.jpg'
-import '../img/FE859BF3-BE14-4E3F-A83C-5D4163248E01.jpg'
-import '../img/IMG_4199.jpg'
-import '../img/IMG_6388.jpg'
-import '../img/IMG_6389.jpg'
-import '../img/IMG_7267.jpg'
-import '../img/IMG_7808.jpg'
-import '../img/IMG_8029.jpg'
-import '../img/IMG_8424.jpg'
-import '../img/IMG_8585.jpg'
 
-export const DesignData = [{
-    img: '2B3A74AF-F7FF-41C5-B435-E3EB2FF30C85'
-}, {
-    img: '18A20FB2-818E-4B72-BE1E-BDECCC52134D'
-}, {
-    img: '29D529C0-9834-4926-A781-C6C881C0A2DE'
-}, {
-    
-    img: '58E5E329-E64F-4972-9392-A6CEAC17A913'
-}, {
-    
-    img: '66B913C7-B3DC-4FE5-93CC-D4AD67EB9379'
-}, {
-    
-    img: '69AFD7B7-FA23-4A54-AB51-7234A6795F2E'
-}, {
-    
-    img: '142B2076-642E-4451-8A54-F00F6822324A'
-}, {
-    
-    img: '567F3275-1455-4384-ABCC-53263531A3E1'
-}, {
-     
-    img: '2562834a-fd18-45d3-b519-9edc954a3acd'
-}, {
-     
-    img: '61556755-B9B9-4DB0-8162-B38A736A693D'
-}, {
-     
-    img: 'A9927117-9F9A-44C2-9BEF-35876E24C7DF'
-}, {
-     
-    img: 'E2F7BDFE-FE72-48E0-B12F-441DEFEA0DC9'
-}, {
-    
-    img: 'FE859BF3-BE14-4E3F-A83C-5D4163248E01'
-}, {
-     
-    img: 'IMG_4199'
-}, {
-     
-    img: 'IMG_6388'
-}, {
-     
-    img: 'IMG_6389'
-}, {
-     
-    img: 'IMG_7267'
-}, {
-     
-    img: 'IMG_7808'
-}, {
-     
-    img: 'IMG_8029'
-}, {
-     
-    img: 'IMG_8424'
-}, {
-     
-    img: 'IMG_8585'
+
+export const DesignData = [ 
+    {
+    id: '01',
+    img: 'pergola/pergola (1).jpeg',
+    type: 'pergola'
+},
+{
+    id: '02',
+    img: 'pergola/pergola (1).jpg',
+    type: ''
+},
+{
+    id: '03',
+    img: 'pergola/pergola (2).jpeg',
+    type: ''
+},
+{
+    id: '04',
+    img: 'pergola/pergola (3).jpeg',
+    type: ''
+},
+{
+    id: '05',
+    img: 'pergola/pergola (4).jpeg',
+    type: ''
+},
+{
+    id: '06',
+    img: 'pergola/pergola (5).jpeg',
+    type: ''
+},
+{
+    id: '07',
+    img: 'pergola/pergola (6).jpeg',
+    type: ''
+},{
+    id: '08',
+    img: 'pergola/pergola (7).jpeg',
+    type: ''
+},{
+    id: '09',
+    img: 'pergola/pergola (8).jpeg',
+    type: ''
+},{
+    id: '10',
+    img: 'pergola/pergola (9).jpeg',
+    type: ''
+},{
+    id: '11',
+    img: 'pergola/pergola (10).jpeg',
+    type: ''
+},{
+    id: '12',
+    img: 'pergola/pergola (11).jpeg',
+    type: ''
+},{
+    id: '13',
+    img: 'pergola/pergola (12).jpeg',
+    type: ''
+},{
+    id: '14',
+    img: 'pergola/pergola (13).jpeg',
+    type: ''
 }]
