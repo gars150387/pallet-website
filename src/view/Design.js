b90198f74661b182781911f14c1758900482f5bb
Gustavo <gar.santeliz@gmail.com>
2022-04-23 13:28:02 -0400

    wrapping button into col to make them responsive and place in center

--- src/view/Design.js
+++ src/view/Design.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Container, Row, Col, Button, ButtonGroup} from 'react-bootstrap';
+import { Container, Row, Col, Button, ButtonGroup } from 'react-bootstrap';
 
 import { DesignTemplate } from '../components/DesignTemplate';
 import { DesignData } from '../components/DesignData';
@@ -14,8 +14,8 @@ export const Design = () => {
     const [value, setvalue] = useState('pergola')
 
 
-        const handleClick = ({ setvalue }) => {
-            setvalue()
+    const handleClick = ({ setvalue }) => {
+        setvalue()
     }
 
     const renderedChoice = DesignData.filter((item) => item.category === value)
@@ -26,12 +26,14 @@ export const Design = () => {
 
             <Container>
                 <Row>
-                    <ButtonGroup onClick={handleClick} size="lg" className="mb-2 mt-3" variant="dark">
-                        <Button id='button' onClick={() => setvalue('pergola')} variant="dark" style={{ border: 'white solid 1px' }}>Pergola</Button>
-                        <Button id='button' onClick={() => setvalue('floor')} variant="dark" style={{ border: 'white solid 1px' }}>Floor</Button>
-                        <Button id='button' onClick={() => setvalue('furnitures')} variant="dark" style={{ border: 'white solid 1px' }}>Furnitures</Button>
-                        <Button id='button' onClick={() => setvalue('adornos')} variant="dark" style={{ border: 'white solid 1px' }}>Decoration</Button>
-                    </ButtonGroup>
+                    <Col md={{ spam: 6, offset: 4}} className='justify-content-center'>
+                        <ButtonGroup id="button-group" onClick={handleClick} size="lg" className="mb-2 mt-3" variant="dark">
+                            <Col sm><Button id='button' onClick={() => setvalue('pergola')} variant="dark" style={{ border: 'white solid 1px' }}>Pergola</Button> </Col>
+                            <Col sm><Button id='button' onClick={() => setvalue('floor')} variant="dark" style={{ border: 'white solid 1px' }}>Floor</Button></Col>
+                            <Col sm><Button id='button' onClick={() => setvalue('furnitures')} variant="dark" style={{ border: 'white solid 1px' }}>Furnitures</Button></Col>
+                            <Col sm><Button id='button' onClick={() => setvalue('adornos')} variant="dark" style={{ border: 'white solid 1px' }}>Decoration</Button></Col>
+                        </ButtonGroup>
+                    </Col>
                 </Row>
                 <ScrollUp />
                 <Row>
@@ -39,7 +41,7 @@ export const Design = () => {
                         {
                             renderedChoice.map((item) => {
 
-                                console.log( 'renderedChoice', renderedChoice )
+                                console.log('renderedChoice', renderedChoice)
 
                                 return (
                                     <DesignTemplate key={item.id} img={item.img} />
