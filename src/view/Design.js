00d3a5bae5559df914ffe8c7634dc977d0f626b6
Gustavo <gar.santeliz@gmail.com>
2022-04-23 00:38:29 -0400

    button to select category set

--- src/view/Design.js
+++ src/view/Design.js
@@ -1,36 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { DesignTemplate } from '../components/DesignTemplate';
 import { DesignData } from '../components/DesignData';
 import { ScrollUp } from '../components/ScrollUp';
 
 import { Container, Row, Col, Button, ButtonGroup } from 'react-bootstrap';
 
+
 export const Design = () => {
+
+    const [value, setvalue] = useState('pergola')
+
+    const handleClick = ({ setvalue }) => {
+
+        switch (value) {
+            case 'furnitures':
+                return value
+            case 'pergola':
+                return value
+            case 'floor':
+                return value
+            case 'adornos':
+                return value;
+                case 'all':
+                return value
+            default:
+        }
+    }
+
+
+
+    const renderedChoice = DesignData.filter((item) => item.category === value)
+
 
     return (
         <div className='designComponent'>
 
             <Container>
                 <Row>
-                    <ButtonGroup size="lg" className="mb-2 mt-3" variant="dark">
-                        <Button variant="light" style={{ border: 'white solid 1px' }}>All</Button>
-                        <Button variant="dark" style={{ border: 'white solid 1px' }}>Furnitures</Button>
-                        <Button variant="dark" style={{ border: 'white solid 1px' }}>Pergola</Button>
-                        <Button variant="dark" style={{ border: 'white solid 1px' }}>Floor</Button>
+                    <ButtonGroup onClick={handleClick} size="lg" className="mb-2 mt-3" variant="dark">
+                        {/* <Button onClick={() => setvalue('all')} variant="light" style={{ border: 'white solid 1px' }}>All</Button> */}
+                        <Button onClick={() => setvalue('furnitures')} variant="dark" style={{ border: 'white solid 1px' }}>Furnitures</Button>
+                        <Button onClick={() => setvalue('pergola')} variant="dark" style={{ border: 'white solid 1px' }}>Pergola</Button>
+                        <Button onClick={() => setvalue('floor')} variant="dark" style={{ border: 'white solid 1px' }}>Floor</Button>
+                        <Button onClick={() => setvalue('adornos')} variant="dark" style={{ border: 'white solid 1px' }}>Decoration</Button>
                     </ButtonGroup>
                 </Row>
                 <ScrollUp />
                 <Row>
                     <Col md={{ span: 6, offset: 3 }} >
+                        {
+                            renderedChoice.map((item) => {
 
-                        {DesignData.map((item) => {
+                                console.log( 'renderedChoice', renderedChoice )
 
-                            const id = Math.floor(Math.random() * 900 + 1)
-
-                            return (
-                                <DesignTemplate key={id} img={item.img} />
-                            )
-                        })}
+                                return (
+                                    <DesignTemplate key={item.id} img={item.img} />
+                                )
+                            })
+                        }
                     </Col>
                 </Row>
             </Container>
