80e4ca24b90a3fc0df026f074979641475ddde11
Gustavo <gar.santeliz@gmail.com>
2022-04-26 23:17:27 -0400

    layout centering component

--- src/view/Contact.js
+++ src/view/Contact.js
@@ -9,7 +9,7 @@ export const Contact = () => {
     return (
         <Container>
             <Row>
-                <Col xs lg={6}>
+                <Col md={{ span: 6, offset: 3 }}>
                     <Row>
                         <Col className="text-center">
                             <h3 id="contact-title">Free quotation and questions <br /> Please contact us.</h3>
