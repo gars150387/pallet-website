fc5863ce464d1446f7c9315f33d4484cc9a453ea
Gustavo <gar.santeliz@gmail.com>
2022-04-27 22:01:09 -0400

    anchor tag added plus style

--- src/components/ReviewFormat.js
+++ src/components/ReviewFormat.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import { GiClick } from 'react-icons/gi'
 
 import '../img/pergola/pergola (21).jpeg'
-import '../img/reviews/Paradiseminipancakes.jpeg'
+import '../img/reviews/paradiseminipancake.jpg'
+
+//src\img\reviews\paradiseminipancake.jpg
 
 
 export const ReviewFormat = () => {
     return (
         <>
-            <Card className="bg-dark text-white" target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/p/CbJUY8fucVp/">
-                <Card.Img src={require('../img/pergola/pergola (21).jpeg')} alt="pergola (21).jpeg" />
-                <Card.ImgOverlay>
-                    <Card.Title>Paradiseminipancakes</Card.Title>
-                    <Card.Text>
-                        <Card.Img src={require('../img/reviews/paradiseminipancakes.jpeg')} alt="paradiseminipancakes.jpeg" />
-                    </Card.Text>
-                </Card.ImgOverlay>
+            <Card className="bg-dark text-white mt-3">
+                <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/p/CbJUY8fucVp/">
+                    <Card.Img src={require('../img/pergola/pergola (21).jpeg')} alt="pergola (21).jpeg" />
+                    <Card.ImgOverlay>
+                        <Card.Title>
+                            <Card.Img style={{ width: '50%', height: '15%', margin: 'auto', position: 'absolute', bottom: '15%', left: '5%', cursor: 'pointer' }} src={require('../img/reviews/paradiseminipancake.jpg')} alt="paradiseminipancakes.jpeg" target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/p/CbJUY8fucVp/" />
+                        </Card.Title>
+                    </Card.ImgOverlay>
+                </a>
             </Card>
         </>
     )
