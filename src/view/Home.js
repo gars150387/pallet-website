e587f1223e6c03a6c2436de360763c6d8697db54
Gustavo <gar.santeliz@gmail.com>
2022-04-29 19:56:31 -0400

    setting up carousel on home

--- src/view/Home.js
+++ src/view/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 
 import { CarouselMain } from '../components/Carousel'
+import '../img/iran/logo/white-logo-transparent-background.png'
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/home.css';
@@ -12,7 +14,7 @@ export const Home = () => {
 	return (
 		<Container className="homePage" id="container-fluid" fluid>
 			<Row>
-				<Col id='col-home' className="animate_animated animate__backInDown" md={{ span: 6, offset: 3 }}>
+				<Col md={{span: 8, offset: 2}} id='col-home' className="animate_animated animate__backInDown">
 					<CarouselMain className='carousel-home' />
 				</Col>
 			</Row>
