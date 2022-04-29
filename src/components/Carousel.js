70bcee9d636360f2cb1d66356868f4473d21b772
Gustavo <gar.santeliz@gmail.com>
2022-04-29 19:55:44 -0400

    adding classnames to set style

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { Carousel, Row, Col, Container, Card } from "react-bootstrap";
+import { Carousel, Row, Card } from "react-bootstrap";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../style/home.css';
+import '../style/carousel.css'
+// import '../style/home.css';
+
 
 import '../img/furnitures/furnitures (12).jpeg'
 import '../img/furnitures/furnitures (26).jpeg'
@@ -12,40 +14,34 @@ import '../img/furnitures/furnitures (4).jpeg'
 
 export const CarouselMain = () => {
 	return (
-		<div>
-			<Container fluid>
-				<Row>
-					<Col >
-						<Carousel fade>
-							
-							<Carousel.Item>
-								<Row>
-									<Card.Img 
-										className="d-block"
-										src={require("../img/furnitures/furnitures (4).jpeg")}
-										alt="" />
-								</Row>
-							</Carousel.Item>
-							<Carousel.Item>
-								<Row>
-									<Card.Img 
-										className="d-block"
-										src={require("../img/furnitures/furnitures (12).jpeg")}
-										alt="" />
-								</Row>
-							</Carousel.Item>
-							<Carousel.Item>
-								<Row>
-									<Card.Img 
-										className="d-block"
-										src={require("../img/furnitures/furnitures (26).jpeg")}
-										alt="" />
-								</Row>
-							</Carousel.Item>
-						</Carousel>
-					</Col>
-				</Row>
-			</Container>
-		</div>
+		<>
+			<Carousel fade>
+				<Carousel.Item >
+					<Row>
+						<Card.Img
+							src={require("../img/furnitures/furnitures (4).jpeg")}
+							alt=""
+							/>
+					</Row>
+				</Carousel.Item>
+				<Carousel.Item>
+					<Row>
+						<Card.Img
+
+							src={require("../img/furnitures/furnitures (12).jpeg")}
+							alt="" />
+					</Row>
+				</Carousel.Item>
+				<Carousel.Item>
+					<Row>
+						<Card.Img
+
+							src={require("../img/furnitures/furnitures (26).jpeg")}
+							alt="" />
+					</Row>
+				</Carousel.Item>
+			</Carousel>
+
+		</>
 	)
 };
