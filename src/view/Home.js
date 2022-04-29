4e18c9f0905e62a6b2664b024c62d9c3061751ea
Gustavo <gar.santeliz@gmail.com>
2022-04-28 23:31:50 -0400

    adding classNames

--- src/view/Home.js
+++ src/view/Home.js
@@ -2,8 +2,6 @@ import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 
 import { CarouselMain } from '../components/Carousel'
-// import { ReviewFormat } from "../components/ReviewFormat";
-// import { CarouselReviews } from "../components/CarouselReviews";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/home.css';
@@ -14,8 +12,8 @@ export const Home = () => {
 	return (
 		<Container className="homePage" id="container-fluid" fluid>
 			<Row>
-				<Col className="animate_animated animate__backInDown" md={{ span: 6, offset: 3 }}>
-					<CarouselMain />
+				<Col id='col-home' className="animate_animated animate__backInDown" md={{ span: 6, offset: 3 }}>
+					<CarouselMain className='carousel-home' />
 				</Col>
 			</Row>
 		</Container>
