18172aa51b5c19200be34e8bfa051bb009a7da70
Gustavo <gar.santeliz@gmail.com>
2022-04-28 21:16:51 -0400

    reviews component added

--- src/view/Home.js
+++ src/view/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Col, Container, Row} from "react-bootstrap";
-// import { CarouselItem1 } from "../components/CarouselItem1";
+import { Col, Container, Row } from "react-bootstrap";
 
 import { CarouselMain } from '../components/Carousel'
+// import { ReviewFormat } from "../components/ReviewFormat";
+import { CarouselReviews } from "../components/CarouselReviews";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/home.css';
@@ -13,13 +14,19 @@ export const Home = () => {
 	return (
 		<Container className="homePage" id="container-fluid" fluid>
 			<Row>
-			<Col className="animate_animated animate__backInDown" md={{ span: 6, offset: 3 }}>
-				<CarouselMain />
-			</Col>	
-			{/* <Col>
-				<CarouselItem1/>
-				</Col> */}
-				</Row>
+				<Col className="animate_animated animate__backInDown" md={{ span: 6, offset: 3 }}>
+					<CarouselMain />
+				</Col>
+				<Col className="animate_animated animate__backInUp" md={{ span: 6, offset: 3 }} style={{marginTop: '2%', marginBottom: '2%'}}>
+					<Col className="text-center">
+						<h3 style={{color: 'white', fontSize: '25px', fontWeight: '500', fontFamily: 'fantasy'}}>These are a few comments from our customers</h3>
+					</Col>
+					<Col>
+						<CarouselReviews/>
+					</Col>
+					
+				</Col>
+			</Row>
 		</Container>
 	)
 };
