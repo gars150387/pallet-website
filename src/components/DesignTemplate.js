1556d4c238fa8266f8db59e656b61c44b69a49ec
Gustavo <gar.santeliz@gmail.com>
2022-04-30 16:44:52 -0400

    added id to bring style to component

--- src/components/DesignTemplate.js
+++ src/components/DesignTemplate.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Card, Col } from 'react-bootstrap';
 
-import "../style/design.css";
-
 export const DesignTemplate = ({ img, id }) => {
     return (
         <Col xs lg={12} className="m-2 justify-content-center animate__animated animate__fadeInDownBig" id="template" key={ id }>
