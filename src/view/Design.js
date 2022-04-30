119780c82256ef650430009d3a641773f1e88ad0
Gustavo <gar.santeliz@gmail.com>
2022-04-30 16:52:49 -0400

    id added to bring style to specific part of the component

--- src/view/Design.js
+++ src/view/Design.js
@@ -26,7 +26,7 @@ export const Design = () => {
 
             <Container>
                 <Row>
-                    <Col md={{ spam: 6, offset: 4}} className='justify-content-center'>
+                    <Col id='button-selector' md={{ spam: 6, offset: 4}} className='justify-content-center'>
                         <ButtonGroup id="button-group" onClick={handleClick} size="lg" className="mb-2 mt-3" variant="dark">
                             <Col sm><Button id='button' onClick={() => setvalue('pergola')} variant="dark" style={{ border: 'white solid 1px' }}>Pergola</Button> </Col>
                             <Col sm><Button id='button' onClick={() => setvalue('floor')} variant="dark" style={{ border: 'white solid 1px' }}>Floor</Button></Col>
@@ -41,9 +41,6 @@ export const Design = () => {
                     <Col style={{paddingRight: '5%'}} sm={6}>
                         {
                             renderedChoice.map((item) => {
-
-                                console.log('renderedChoice', renderedChoice)
-
                                 return (
                                     <DesignTemplate key={item.id} img={item.img} />
                                 )
